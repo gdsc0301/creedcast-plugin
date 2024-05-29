07ad7804d2d98eeae08d94a0809b8a2faf9d07e7
Guilherme Carvalho <gdsc0301@gmail.com>
2024-05-29 02:19:41 -0300

    chore: Update .gitignore to exclude 'vendor' folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'guilherme/creed-cast',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f574299bcd234e82cf2847f31f742c35ff1f54ec',
+        'type' => 'plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'guilherme/creed-cast' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f574299bcd234e82cf2847f31f742c35ff1f54ec',
+            'type' => 'plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
